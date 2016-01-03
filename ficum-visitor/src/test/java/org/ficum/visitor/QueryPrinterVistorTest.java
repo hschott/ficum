e60231fae8f1b16c983767eccbdd7ba5768ea548
Holger Balow-Schott <holger.balow-schott@t-systems-mms.com>
2016-01-03 21:53:14 +0100

    use ParseHelper

--- ficum-visitor/src/test/java/org/ficum/visitor/QueryPrinterVistorTest.java
+++ ficum-visitor/src/test/java/org/ficum/visitor/QueryPrinterVistorTest.java
@@ -1,35 +1,17 @@
 package org.ficum.visitor;
 
-import java.util.Deque;
-
-import org.ficum.node.Builder;
 import org.ficum.node.Node;
-import org.ficum.parser.ConstraintParser;
-import org.ficum.parser.ExpressionParser;
+import org.ficum.parser.ParseHelper;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
-import org.parboiled.Parboiled;
-import org.parboiled.parserunners.BasicParseRunner;
-import org.parboiled.support.ParsingResult;
 
 public class QueryPrinterVistorTest {
-    private BasicParseRunner<Deque<Object>> parseRunner;
-
-    @Before
-    public void setUp() {
-        String[] allowedPaths = { "first", "second", "third", "fourth", "fifth" };
-        ConstraintParser parser = Parboiled.createParser(ExpressionParser.class, (Object) allowedPaths);
-
-        parseRunner = new BasicParseRunner<Deque<Object>>(parser.root());
-    }
+    private String[] allowedSelectorNames = { "first", "second", "third", "fourth", "fifth" };
 
     @Test
     public void testDate() {
         String input = "first==2015-12-29";
-        ParsingResult<Deque<Object>> result = parseRunner.run(input);
-        Node node = Builder.build(result.resultValue);
-
+        Node node = ParseHelper.parse(input, allowedSelectorNames);
         String actual = new QueryPrinterVisitor().start(node);
 
         Assert.assertEquals(input, actual);
@@ -38,9 +20,7 @@ public class QueryPrinterVistorTest {
     @Test
     public void testDateBC() {
         String input = "first==-0650-12-29";
-        ParsingResult<Deque<Object>> result = parseRunner.run(input);
-        Node node = Builder.build(result.resultValue);
-
+        Node node = ParseHelper.parse(input, allowedSelectorNames);
         String actual = new QueryPrinterVisitor().start(node);
 
         Assert.assertEquals(input, actual);
@@ -49,9 +29,7 @@ public class QueryPrinterVistorTest {
     @Test
     public void testNaturalOrder() {
         String input = "first==1l,second=gt='two';third=le=3.0";
-        ParsingResult<Deque<Object>> result = parseRunner.run(input);
-        Node node = Builder.build(result.resultValue);
-
+        Node node = ParseHelper.parse(input, allowedSelectorNames);
         String actual = new QueryPrinterVisitor().start(node);
 
         Assert.assertEquals(input, actual);
@@ -60,9 +38,7 @@ public class QueryPrinterVistorTest {
     @Test
     public void testNestedPreceded() {
         String input = "first==1l,((second=gt='two';third=le=3.0);fourth=lt='five')";
-        ParsingResult<Deque<Object>> result = parseRunner.run(input);
-        Node node = Builder.build(result.resultValue);
-
+        Node node = ParseHelper.parse(input, allowedSelectorNames);
         String actual = new QueryPrinterVisitor().start(node);
 
         Assert.assertEquals(input, actual);
@@ -71,9 +47,7 @@ public class QueryPrinterVistorTest {
     @Test
     public void testPrecededOrder() {
         String input = "(first==1l;second=gt='two'),(third=le=3.0;fourth=lt='five')";
-        ParsingResult<Deque<Object>> result = parseRunner.run(input);
-        Node node = Builder.build(result.resultValue);
-
+        Node node = ParseHelper.parse(input, allowedSelectorNames);
         String actual = new QueryPrinterVisitor().start(node);
 
         Assert.assertEquals(input, actual);
@@ -82,9 +56,7 @@ public class QueryPrinterVistorTest {
     @Test
     public void testTimestampBC() {
         String input = "first==-0456-03-19T18:34:12.000Z";
-        ParsingResult<Deque<Object>> result = parseRunner.run(input);
-        Node node = Builder.build(result.resultValue);
-
+        Node node = ParseHelper.parse(input, allowedSelectorNames);
         String actual = new QueryPrinterVisitor().start(node);
 
         Assert.assertEquals(input, actual);
@@ -93,9 +65,7 @@ public class QueryPrinterVistorTest {
     @Test
     public void testTimestampZoneOffset() {
         String input = "first==2015-12-29T18:34:12.000+05:00";
-        ParsingResult<Deque<Object>> result = parseRunner.run(input);
-        Node node = Builder.build(result.resultValue);
-
+        Node node = ParseHelper.parse(input, allowedSelectorNames);
         String actual = new QueryPrinterVisitor().start(node);
 
         Assert.assertEquals(input, actual);
@@ -104,9 +74,7 @@ public class QueryPrinterVistorTest {
     @Test
     public void testTimestampZulu() {
         String input = "first==2015-12-29T18:34:12.000Z";
-        ParsingResult<Deque<Object>> result = parseRunner.run(input);
-        Node node = Builder.build(result.resultValue);
-
+        Node node = ParseHelper.parse(input, allowedSelectorNames);
         String actual = new QueryPrinterVisitor().start(node);
 
         Assert.assertEquals(input, actual);
